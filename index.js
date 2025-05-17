63429d669ee622b7143cbf3e0fe03a10b7f6c9aa
BuiltByWillDev <builtbywill@proton.me>
2025-05-16 18:15:58 -0600

    fully responsive and finishing touches added!

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1 / 20
+
+ function moveBackground(event) {
+  const shapes = document.querySelectorAll( " .shape");
+  const x = event.clientX * scaleFactor
+  const y = event.clientY * scaleFactor
+
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+
+
+}
+
+
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle
+  if (contrastToggle) {
+  document.body.classList += " dark-theme"
+  }
+else{
+  document.body.classList.remove("dark-theme")
+}
+}
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -26,7 +58,7 @@ function contact(event) {
   }).showToast();
 }
 
-let isModalOpen = false
+
 function toggleModal(){
     if (isModalOpen) {
         isModalOpen = false
